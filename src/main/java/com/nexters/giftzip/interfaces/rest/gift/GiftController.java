aabc8f6241db9d7f27dd12355177131cb0441620
지혜 <youo78@naver.com>
2021-01-15 23:37:23 +0900

    swagger 설정 추가

---
+++ src/main/java/com/nexters/giftzip/interfaces/rest/gift/GiftController.java
@@ -0,0 +1,13 @@
+package com.nexters.giftzip.interfaces.rest.gift;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GiftController {
+
+    @GetMapping("/")
+    public void temp() {
+
+    }
+}
