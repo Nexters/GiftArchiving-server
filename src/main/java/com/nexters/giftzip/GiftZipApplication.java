2fad8d17455c7df65ae2d9146cb9fc73f0ffac12
지혜 <youo78@naver.com>
2021-01-15 22:29:05 +0900

    초기 셋팅

---
+++ src/main/java/com/nexters/giftzip/GiftZipApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.giftzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftZipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftZipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nexters/giftzip/GiftZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.giftzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftZipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
