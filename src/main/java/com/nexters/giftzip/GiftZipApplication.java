94f19bb162079bd589be7bb3d1147f15ea6280cf
지혜 <youo78@naver.com>
2021-02-03 21:59:27 +0900

    포트 변경

--- src/main/java/com/nexters/giftzip/GiftZipApplication.java
+++ src/main/java/com/nexters/giftzip/GiftZipApplication.java
@@ -5,17 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @Slf4j
 @PropertySource("classpath:aws.properties")
 public class GiftZipApplication {
-    @Value("${spring.profiles.active}")
-    private static String activeProfile;
 
     public static void main(String[] args) {
         SpringApplication.run(GiftZipApplication.class, args);
-        log.info("ACTIVE PROFILE :: {}", activeProfile);
     }
 }
