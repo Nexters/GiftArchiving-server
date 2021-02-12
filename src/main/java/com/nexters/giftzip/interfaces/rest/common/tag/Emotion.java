62170cf580c1fd690029c9dd10bcc797b9fb78ce
지혜 <youo78@naver.com>
2021-02-12 14:02:03 +0900

    - Emotion 태그 값 추가 - 선물목록 응답값 태그, 받은 날짜 추가

--- src/main/java/com/nexters/giftzip/interfaces/rest/common/tag/Emotion.java
+++ src/main/java/com/nexters/giftzip/interfaces/rest/common/tag/Emotion.java
@@ -1,5 +1,19 @@
 package com.nexters.giftzip.interfaces.rest.common.tag;
 
 public enum Emotion {
-    GOOD
+    CHEER("응원해"),
+    SORRY("미안해"),
+    BOAST("나최고지"),
+    CONGRATULATIONS("축하해"),
+    NICE_SENSE("센스최고"),
+    LOVE_YOU("사랑해"),
+    IMPRESSION("감동이야"),
+    AMAZING("놀라워")
+    ;
+
+    private String desc;
+
+    Emotion(String desc) {
+        this.desc = desc;
+    }
 }
--- src/main/java/com/nexters/giftzip/interfaces/rest/gift/dto/GiftListDto.java
+++ src/main/java/com/nexters/giftzip/interfaces/rest/gift/dto/GiftListDto.java
@@ -1,7 +1,12 @@
 package com.nexters.giftzip.interfaces.rest.gift.dto;
 
+import com.nexters.giftzip.interfaces.rest.common.tag.Category;
+import com.nexters.giftzip.interfaces.rest.common.tag.Emotion;
+import com.nexters.giftzip.interfaces.rest.common.tag.Reason;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class GiftListDto {
@@ -9,14 +14,23 @@ public class GiftListDto {
     private String imgUrl;
     private String name;
     private String content;
+    private LocalDateTime receiveDate;
+    private Category category;
+    private Emotion emotion;
+    private Reason reason;
 
     public GiftListDto() { }
 
     @Builder
-    public GiftListDto(String id, String imgUrl, String name, String content) {
+    public GiftListDto(String id, String imgUrl, String name, String content, LocalDateTime receiveDate, Category category,
+                       Emotion emotion, Reason reason) {
         this.id = id;
         this.imgUrl = imgUrl;
         this.name = name;
         this.content = content;
+        this.receiveDate = receiveDate;
+        this.category = category;
+        this.emotion = emotion;
+        this.reason = reason;
     }
 }
