1527f4ce9b7ea3db14c0f27d8abd2161b912196f
지혜 <youo78@naver.com>
2021-02-14 02:37:20 +0900

    - 불필요한 임포트 제거

--- src/main/java/com/nexters/giftzip/config/SecurityConfiguration.java
+++ src/main/java/com/nexters/giftzip/config/SecurityConfiguration.java
@@ -1,13 +1,10 @@
 package com.nexters.giftzip.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
